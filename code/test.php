64e1410ae14dac5d8164c9dc46d00407278916b3
wei840222 <wei840222@gmail.com>
2019-06-08 01:20:43 +0800

    add test mysql

--- code/test.php
+++ code/test.php
@@ -1,2 +1,5 @@
 <?php
-a
+    $mysqli = new mysqli("mysql", "root", "root", "test");
+    $name=substr(md5(rand()),0,6);
+    $result = $mysqli->query("INSERT INTO `test` (`id`, `name`, `gender`) VALUES (NULL, '".$name."', '1')");
+?>
