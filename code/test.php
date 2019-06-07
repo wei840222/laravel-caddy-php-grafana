9893f920b82ad110f4d78fb0f767bc6d852af805
wei840222 <wei840222@gmail.com>
2019-06-08 04:21:29 +0800

    init laravel

--- code/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $mysqli = new mysqli("mysql", "root", "root", "test");
-    $name=substr(md5(rand()),0,6);
-    $result = $mysqli->query("INSERT INTO `test` (`id`, `name`, `gender`) VALUES (NULL, '".$name."', '1')");
-?>
